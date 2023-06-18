e0df6c415af15adc656d5e091a635b3cf8314f81
David Kirby <davidkirbymusician@gmail.com>
2023-06-18 22:39:34 +0000

    initial commit after cloning.

--- run.py
+++ run.py
@@ -1,3 +1,39 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import numpy as np
+
+
+class player:  
+    def __init__(self):
+        self.picks = []
+        self.board = np.full((5, 5), "*")
+
+player1 = player()
+player2 = player()
+
+print(player1.board)
+print("\n --------------------- \n")
+print(player2.board)
+
+
+
+# def game_initialize():
+#     player1 = player()
+#     player2 = player()
+#     player1.construct_board()
+#     player2.construct_board()
+#     return player1, player2
+
+
+# def update_UI():
+#     print(np.array(player1.board))
+#     print(np.array(player2.board))
+
+
+
+
+
+
+
+
+# player1, player2 = game_initialize()
+# get_user_picks()
+# # update_UI()
