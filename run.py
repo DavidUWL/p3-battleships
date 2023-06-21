d65b57624ee140d5e39a22eae94a8e7107a62903
David Kirby <davidkirbymusician@gmail.com>
2023-06-21 21:14:12 +0000

    function completed for taking coordinates from player and assigning them to board

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+
 import numpy as np
 
 
@@ -5,6 +6,7 @@ class player:
     def __init__(self):
         self.boat_coordinates = []
         self.board = np.full((5, 5), "*")
+
 
 player1 = player()
 
@@ -21,10 +23,12 @@ def update_UI():
 def get_boat_coordinates():
     i = 1
     while i < 6:
-        y = int(input("pick a row from 1-5:"))
+        y = int(input("pick a row from 1-5:")) 
         y = coordinate_validation(y)
-        x = int(input("pick a column from 1-5:"))
+        x = int(input("pick a column from 1-5:")) 
         x = coordinate_validation(x)
+        y = y - 1 # subtracting 1 for zero indexing
+        x = x - 1 # subtracting 1 for zero indexing
         c = [y, x]
         player1.boat_coordinates.append(c)
         i += 1
@@ -34,10 +38,22 @@ def get_boat_coordinates():
 def coordinate_validation(value):
     while value < 1 or value > 5:
         print("Choose between 1-5.")
-        value = int(input("pick a row from 1-5:"))
+        value = int(input("pick a row from 1-5:")) - 1
     return value
 
 
+def plot_coordinates(player, player_coordinates):
+    converted_player_coordinates = np.array(player_coordinates) # converts board to numpy array
+
+    for i in range(converted_player_coordinates.shape[0]):
+        row = converted_player_coordinates[i, 0]
+        column = converted_player_coordinates[i, 1]
+        player.board[row, column] = "#"
+
+    return player.board
+
+    
 update_UI()
 get_boat_coordinates()
-
+plot_coordinates(player1, player1.boat_coordinates)
+update_UI()
