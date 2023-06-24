38445d61a36f01933926213cf8fa7dc89045526a
David Kirby <davidkirbymusician@gmail.com>
2023-06-24 18:00:44 +0000

    created call artillery function and a function to check if it was a hit

--- run.py
+++ run.py
@@ -4,8 +4,25 @@ import numpy as np
 
 class player:  
     def __init__(self):
-        self.boat_coordinates = []
+        self.boat_coordinates = np.empty((0, 2))
+        self.artillery_coordinates = np.empty((0, 2))
         self.board = np.full((5, 5), "*")
+        self.boat = {
+            "boat1": {
+                "icon": "#",
+                "damaged_icon": "X",
+                "length": 4,
+                "hit": False
+            },
+
+            "boat2": {
+                "icon": "@",
+                "damaged_icon": "X",
+                "length": 2,
+                "hit": False
+            }
+        }
+ 
 
 
 player1 = player()
@@ -15,9 +32,11 @@ player2 = player()
 
 
 def update_UI():
-    print(player1.board)
-    print("\n --------------------- \n")
+    check_hits(player1)
+    check_hits(player2)
     print(player2.board)
+    print("\n --------------------- \n")
+    print(player1.board)
 
 
 def get_boat_coordinates():
@@ -27,12 +46,12 @@ def get_boat_coordinates():
         y = coordinate_validation(y)
         x = int(input("pick a column from 1-5:")) 
         x = coordinate_validation(x)
-        y = y - 1 # subtracting 1 for zero indexing
-        x = x - 1 # subtracting 1 for zero indexing
-        c = [y, x]
-        player1.boat_coordinates.append(c)
+        y = y - 1  # subtracting 1 for zero indexing
+        x = x - 1  # subtracting 1 for zero indexing
+        c = np.array([[y, x]])
+        player1.boat_coordinates = np.append(player1.boat_coordinates, c, axis=0)
         i += 1
-    print(player1.boat_coordinates)
+    return player1.boat_coordinates
 
 
 def coordinate_validation(value):
@@ -42,18 +61,43 @@ def coordinate_validation(value):
     return value
 
 
-def plot_coordinates(player, player_coordinates):
-    converted_player_coordinates = np.array(player_coordinates) # converts board to numpy array
-
-    for i in range(converted_player_coordinates.shape[0]):
-        row = converted_player_coordinates[i, 0]
-        column = converted_player_coordinates[i, 1]
-        player.board[row, column] = "#"
-
+def plot_coordinates(player, boat_coordinates):
+    for i in range(boat_coordinates.shape[0]):
+        row = int(boat_coordinates[i, 0])
+        column = int(boat_coordinates[i, 1])
+        player.board[row, column] = player.boat["boat1"]["icon"]
+    # print(player.boat_coordinates)
     return player.board
 
+
+def call_artillery(player, artillery_coordinates):
+    print("Call your artillery shot!")
+    y = int(input("pick a row from 1-5:")) 
+    y = coordinate_validation(y)
+    x = int(input("pick a column from 1-5:")) 
+    x = coordinate_validation(x)
+    y = y - 1  # subtracting 1 for zero indexing
+    x = x - 1  # subtracting 1 for zero indexing
+    c = np.array([[y, x]])
+    player.artillery_coordinates = np.append(player.artillery_coordinates, c, axis=0)
+    # print(player1.artillery_coordinates)
+
     
+def check_hits(player):
+    direct_hits = np.where(np.all(player.artillery_coordinates == player.boat_coordinates[:, None], axis=2))
+    for i in range(len(direct_hits[0])):
+        row = int(direct_hits[0][i])
+        column = int(direct_hits[1][i])
+        player.board[row, column] = player.boat["boat1"]["damaged_icon"]
+    return player.board
+
+
+
 update_UI()
 get_boat_coordinates()
+# print(player1.boat_coordinates)
 plot_coordinates(player1, player1.boat_coordinates)
 update_UI()
+call_artillery(player1, player1.artillery_coordinates)
+update_UI()
+
