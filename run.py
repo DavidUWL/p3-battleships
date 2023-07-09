a08a38dd7ee43dfaf335815285f42ad2c45f3c10
David Kirby <davidkirbymusician@gmail.com>
2023-07-09 15:41:06 +0000

    fixed error where computer artillery was not formatted in the 1-5 row/column format.

--- run.py
+++ run.py
@@ -195,7 +195,7 @@ def new_game():
             if player.artillery_coordinates[y, x] != "!":
                 player.artillery_coordinates[y, x] = "!"
                 break
-        print(f"Computer fires at: {y}, {x}")
+        print(f"Computer fires at: {y + 1}, {x + 1}")
         time.sleep(1)  # 1 second wait time before code executes to delay gameplay
         return player.artillery_coordinates
 
